7ffe09a2016ce8b4a27d66a98694af33e687dd63
Milan Somsedík <42219550+somsac@users.noreply.github.com>
2020-07-24 10:39:24 +0200

    Delete last_update.js

--- .github/actions/last_update.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var date = new Date();
-
-fs.writeFile('update.json', '{"update":"' + date.toISOString() + '"}', function (err) {
-  if (err) throw err;
-  console.log('Replaced!');
-});
